e7d61b8641b53638d20020071de8f88ca13a8986
IngridMorones20 <54509013+IngridMorones20@users.noreply.github.com>
2019-10-17 18:54:04 -0500

    Update main.c

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc,char *argv[]) {
-    double num1, num2, suma, resta, mult, div;
+    double num1, num2, suma, resta, mult, divi;
     num1 = atof(argv[1]);
     num2 = atof(argv[2]);
     suma = num1+num2;
     resta = num1-num2;
     mult = num1*num2;
-    div = num1/num2;
+    divi = num1/num2;
     printf("%.2f\n", suma);
     printf("%.2f\n", resta);
     printf("%.2f\n", mult);
-    printf("%.2f\n", div);
+    printf("%.2f\n", divi);
     return 0;
 }
